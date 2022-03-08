c85124fa3799042416081db48efaf9375255bd7e
Ruovan <2454234258@qq.com>
2022-03-08 21:35:25 +0800

    docs📝:  更新注释

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -7,7 +7,8 @@ import { patch } from './vdom/patch.js'
 export function mountComponent(vm, el) {
   // 先将 el 赋值给 vm 的 $el 属性
   vm.$el = el
-  // 然后调用 _render 方法来调用模板编译过程中生成的　render 函数
+
+  // 然后调用 _render 方法来调用模板编译过程中生成的 render 函数
   // -- 用于生成 虚拟DOM
   let vDom = vm._render()
 
@@ -23,6 +24,7 @@ export function lifecycleMixin(Vue) {
     // -- 初始化渲染、更新渲染都通过这个 patch 方法
     // -- 该方法会返回一个真实的 DOM
     // -- 需要将其赋值给原来的节点，用于渲染到页面上
+    // -- 该方法接收两个参数，第一个参数为 旧节点， 第二个参数为 新节点——即上面生成的 虚拟DOM
     vm.$el = patch(vm.$el, vnode)
   }
 }
